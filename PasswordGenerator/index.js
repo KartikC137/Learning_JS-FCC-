7a01fb450654f2cb2c61d5f5200537c0e1ce63f7
KartikC137 <119693639+KartikC137@users.noreply.github.com>
2024-08-06 01:30:35 +0530

    Completed Password Generator app

--- PasswordGenerator/index.js
+++ PasswordGenerator/index.js
@@ -1,8 +1,17 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let contentEl = document.getElementById("content")
+let contentEl = document.getElementById("content-el");
 
-function generatePassword() {
-    contentEl.textContent = characters
+function generateRandomPassword() {
+
 }
+function logOutPassword() {
+    let storeRandomNumber="";
+    for(let i = 0; i < 16; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length);
+        storeRandomNumber += characters[randomIndex];
+    }
+    contentEl.textContent = storeRandomNumber;
+}
+
